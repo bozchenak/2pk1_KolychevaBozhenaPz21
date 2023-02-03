44ef42b28192a322fded968ed21323a1bd16d3e0
DeanChine <DeanChine@DESKTOP-KVGI39A>
2023-02-03 21:31:23 +0500

    Добавьте файлы проекта.

---
+++ klass/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klass
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+        }
+    }
+
+    class ComplexNumber
+    {
+        public int a ;
+        public int b;
+        public int c;
+        
+
+        static void PrintComplexNumber() 
+        {
+            ComplexNumber complexNumber = new ComplexNumber();
+            complexNumber.a = Convert.ToInt32(Console.ReadLine());
+            complexNumber.b = Convert.ToInt32(Console.ReadLine());
+           
+            Console.WriteLine($"Opisaniye : {complexNumber.a}+{complexNumber.b}+i");
+            Console.ReadKey();
+        }
+
+        static void GetMultiplicationByNumber() 
+        {
+            ComplexNumber complexNumber1 = new ComplexNumber();
+            complexNumber1.a = Convert.ToInt32(Console.ReadLine());
+            complexNumber1.b = Convert.ToInt32(Console.ReadLine());
+            int ymA = complexNumber1.a*complexNumber1.c;
+            int ymB = complexNumber1.b*complexNumber1.c;
+            complexNumber1.c = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"Ymnojenie : {ymA}+{ymB}+i");
+        }
+
+    }
+}
+
+
+
